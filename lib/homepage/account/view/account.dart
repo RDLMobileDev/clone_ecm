be71891a430e6125014ccd45e34add70df54ec20
fikibrahim <taufikibrahim21@gmail.com>
2021-11-18 13:27:36 +0700

    +layouting account

--- lib/homepage/account/view/account.dart
+++ lib/homepage/account/view/account.dart
@@ -11,60 +11,116 @@ class _AccountMemberState extends State<AccountMember> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.fromLTRB(16, 28, 24, 16),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage(
-                      "assets/images/sudin.png",
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "Sudin",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blueAccent),
-                            ),
-                            TextSpan(text: " "),
-                            TextSpan(
-                              text: "Was Aproved yout E-CM Card",
-                              style:
-                                  TextStyle(fontSize: 14, color: Colors.grey),
-                            )
-                          ],
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: EdgeInsets.fromLTRB(16, 28, 24, 16),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 24),
+                      width: 74,
+                      height: 74,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(
+                          "assets/images/budi.png",
                         ),
                       ),
-                      Text(
-                        "1 hour ago",
-                        style: TextStyle(fontSize: 14, color: Colors.grey),
-                      )
-                    ],
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Budi",
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "budi@sugity.com - (member)",
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(
+                  height: 29,
+                  thickness: 2,
+                  color: Color(0xffE3E5E5),
+                ),
+                Container(
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 40.0,
+                              height: 40.0,
+                              margin: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    child: Icon(
+                                      Icons.logout,
+                                      size: 28,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Logout",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xffFF0000),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  "Leave the app",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_right,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
