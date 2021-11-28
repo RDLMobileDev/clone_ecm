2bc0a940320ff860a1413581ca75ab5a9eea55f8
rizal282 <rizalarya1@gmail.com>
2021-11-28 21:14:48 +0700

    + hide list TM

--- lib/homepage/home/view/home.dart
+++ lib/homepage/home/view/home.dart
@@ -6,6 +6,7 @@ import 'package:e_cm/homepage/home/fillnew/fillnew.dart';
 import 'package:e_cm/homepage/home/listname/listname.dart';
 import 'package:e_cm/homepage/home/model/historyecmmodel.dart';
 import 'package:e_cm/homepage/home/services/historyecmservice.dart';
+import 'package:e_cm/homepage/notification/view/notification.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -264,8 +265,10 @@ class _HomeState extends State<Home> {
             ),
             InkWell(
               onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ApprovedEcm()));
+                // Navigator.of(context).push(
+                //     MaterialPageRoute(builder: (context) => ApprovedEcm()));
+                    Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => NotificationMember()));
               },
               child: Container(
                 margin: const EdgeInsets.only(
@@ -301,42 +304,42 @@ class _HomeState extends State<Home> {
             SizedBox(
               height: 16,
             ),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ListTmName()));
-              },
-              child: Container(
-                margin: const EdgeInsets.only(
-                  left: 16,
-                  right: 16,
-                ),
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                width: MediaQuery.of(context).size.width,
-                height: 40,
-                decoration: BoxDecoration(
-                    color: Color(0xFF00AEDB),
-                    borderRadius: BorderRadius.all(Radius.circular(5))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      "List TM Name",
-                      style: TextStyle(
-                          fontFamily: 'Rubik',
-                          color: Colors.white,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      size: 14,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ),
+            // InkWell(
+            //   onTap: () {
+            //     Navigator.of(context).push(
+            //         MaterialPageRoute(builder: (context) => ListTmName()));
+            //   },
+            //   child: Container(
+            //     margin: const EdgeInsets.only(
+            //       left: 16,
+            //       right: 16,
+            //     ),
+            //     padding: const EdgeInsets.only(left: 16, right: 16),
+            //     width: MediaQuery.of(context).size.width,
+            //     height: 40,
+            //     decoration: BoxDecoration(
+            //         color: Color(0xFF00AEDB),
+            //         borderRadius: BorderRadius.all(Radius.circular(5))),
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       children: const [
+            //         Text(
+            //           "List TM Name",
+            //           style: TextStyle(
+            //               fontFamily: 'Rubik',
+            //               color: Colors.white,
+            //               fontSize: 12,
+            //               fontWeight: FontWeight.w400),
+            //         ),
+            //         Icon(
+            //           Icons.arrow_forward_ios,
+            //           size: 14,
+            //           color: Colors.white,
+            //         )
+            //       ],
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
