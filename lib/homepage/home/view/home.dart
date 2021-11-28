33e2fefe71484256368b71e8765366c3f78f337d
Hirarki Wendri <ventus.kiki@gmail.com>
2021-11-28 14:13:32 +0700

    + update history ecm home

--- lib/homepage/home/view/home.dart
+++ lib/homepage/home/view/home.dart
@@ -4,6 +4,8 @@ import 'package:e_cm/homepage/home/approved/approved.dart';
 import 'package:e_cm/homepage/home/component/sliderhistory.dart';
 import 'package:e_cm/homepage/home/fillnew/fillnew.dart';
 import 'package:e_cm/homepage/home/listname/listname.dart';
+import 'package:e_cm/homepage/home/model/historyecmmodel.dart';
+import 'package:e_cm/homepage/home/services/historyecmservice.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,6 +20,8 @@ class _HomeState extends State<Home> {
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   String userName = "";
 
+  List<HistoryEcmModel> _listHistoryEcmUser = [];
+
   Future<String> getNameUser() async {
     final SharedPreferences prefs = await _prefs;
     String nameUser = prefs.getString("usernameKey").toString();
@@ -27,10 +31,24 @@ class _HomeState extends State<Home> {
     return nameUser;
   }
 
+  Future<List<HistoryEcmModel>> getHistoryEcmByUser() async {
+    final SharedPreferences prefs = await _prefs;
+    String idUser = prefs.getString("idKeyUser").toString();
+    String tokenUser = prefs.getString("tokenKey").toString();
+
+    var dataEcmHistory =
+        await historyEcmService.getHistoryEcmModel(idUser, tokenUser);
+
+    _listHistoryEcmUser = dataEcmHistory;
+    print(_listHistoryEcmUser);
+    return dataEcmHistory;
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getHistoryEcmByUser();
     getNameUser();
   }
 
@@ -128,12 +146,61 @@ class _HomeState extends State<Home> {
                 right: 16,
               ),
               width: MediaQuery.of(context).size.width,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [SliderHistory(), SliderHistory()],
-                ),
+              child: FutureBuilder(
+                future: getHistoryEcmByUser(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 130,
+                        child: Center(
+                          child: Text(
+                            "Loading history...",
+                            style: TextStyle(
+                                fontFamily: 'Rubik',
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF979C9E)),
+                          ),
+                        ));
+                  } else {
+                    if (_listHistoryEcmUser.isEmpty) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        padding: const EdgeInsets.all(16),
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        height: 130,
+                        child: Center(
+                            child: Text("No history here",
+                                style: TextStyle(
+                                    fontFamily: 'Rubik',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF404446)))),
+                      );
+                    } else {
+                      return Container(
+                        height: 130,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _listHistoryEcmUser.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, i) {
+                            return SliderHistory(
+                              classificationName:
+                                  _listHistoryEcmUser[i].classification,
+                              costRp: _listHistoryEcmUser[i].totalHarga,
+                              factoryPlace: _listHistoryEcmUser[i].lokasi,
+                              tanggal: _listHistoryEcmUser[i].date,
+                              itemsRepair:
+                                  _listHistoryEcmUser[i].arrayitemrepair,
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  }
+                },
               ),
             ),
             SizedBox(
