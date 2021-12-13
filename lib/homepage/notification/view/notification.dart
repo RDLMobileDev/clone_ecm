23115d4ff9abdf0a44e466024e1fad403192291b
Hirarki Wendri <ventus.kiki@gmail.com>
2021-12-13 18:00:56 +0700

    + update notifikasi

--- lib/homepage/notification/view/notification.dart
+++ lib/homepage/notification/view/notification.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, duplicate_ignore
 
+import 'package:e_cm/homepage/notification/model/notifmodel.dart';
 import 'package:e_cm/homepage/notification/services/apinotif.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/painting.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -15,24 +15,22 @@ class NotificationMember extends StatefulWidget {
 
 class _NotificationMemberState extends State<NotificationMember> {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  List<NotificationModel> listNotificationEcm = [];
 
-  List listNotif = [];
-
-  Future getListNotifFromService() async {
+  Future getListNotif() async {
     final prefs = await _prefs;
     String tokenUser = prefs.getString("tokenKey").toString();
     String idUser = prefs.getString("idKeyUser").toString();
 
-    listNotif = await notifikasiService.getNotificationData(tokenUser, idUser);
+    listNotificationEcm =
+        await notifikasiService.getNotificationData(tokenUser, idUser);
 
-    print("notif data -> $listNotif");
-
-    return await notifikasiService.getNotificationData(tokenUser, idUser);
+    return notifikasiService.getNotificationData(tokenUser, idUser);
   }
 
   @override
   void initState() {
-    getListNotifFromService();
+    getListNotif();
     super.initState();
   }
 
@@ -65,76 +63,60 @@ class _NotificationMemberState extends State<NotificationMember> {
             color: Colors.black54,
           ),
           Container(
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.all(16),
+            padding: EdgeInsets.only(left: 16, top: 16, bottom: 8),
             child: FutureBuilder(
-              future: getListNotifFromService(),
+              future: getListNotif(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Center(
-                      child: Text(
-                        "Loading List Notification...",
-                        style: TextStyle(
-                          fontFamily: 'Rubik',
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  );
+                  return Text("Memuat notifikasi...");
                 }
 
                 return ListView.builder(
                   shrinkWrap: true,
-                  itemCount: listNotif.length,
+                  itemCount: listNotificationEcm.length,
                   itemBuilder: (context, i) {
-                    return Container(
-                      padding: const EdgeInsets.only(top: 8, bottom: 8),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(color: Color(0xFFE3E5E5)))),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 48,
-                            height: 48,
-                            decoration: BoxDecoration(
-                                color: Color(0xFF00AEDB),
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                    image:
-                                        AssetImage("assets/images/ario.png"))),
-                          ),
-                          SizedBox(
-                            width: 16,
-                          ),
-                          RichText(
-                            text: TextSpan(
-                              style: TextStyle(
-                                fontFamily: 'Rubik',
-                                fontSize: 16,
-                              ),
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text: listNotif[i]['nama'],
+                    return Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/images/sudin.png"),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14.0,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: listNotificationEcm[i].nama,
                                     style: TextStyle(
-                                        color: Color(0xFF00AEDB),
-                                        fontWeight: FontWeight.w700)),
-                                TextSpan(
-                                    text: ' - ',
-                                    style: TextStyle(color: Color(0xFF00AEDB))),
-                                TextSpan(
-                                    text: listNotif[i]['status'],
-                                    style: TextStyle(color: Color(0xFF00AEDB))),
-                              ],
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blueAccent),
+                                  ),
+                                  TextSpan(text: " "),
+                                  TextSpan(
+                                    text: "Was Aproved yout E-CM Card",
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
+                            Text(
+                              listNotificationEcm[i].waktu,
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ],
                     );
                   },
                 );
