18af601c49de61b1e8dcf4c73b1d815684368954
Reynaldo Ferdian Saputra <rferdian516@gmail.com>
2022-01-07 15:42:07 +0700

    add time variable on approved e-sign

--- lib/homepage/home/model/approvedmodel.dart
+++ lib/homepage/home/model/approvedmodel.dart
@@ -3,21 +3,25 @@ class ApprovedModel {
   String? nama;
   String? foto;
   String? status;
+  String? waktu;
 
-  ApprovedModel({this.notifEcmId, this.nama, this.foto, this.status});
+  ApprovedModel(
+      {this.notifEcmId, this.nama, this.foto, this.status, this.waktu});
 
   factory ApprovedModel.fromJson(Map<String, dynamic> json) {
     return ApprovedModel(
         notifEcmId: json['notifecm_id'].toString(),
         nama: json['nama'].toString(),
         foto: json['foto'].toString(),
-        status: json['status'].toString());
+        status: json['status'].toString(),
+        waktu: json['waktu'].toString());
   }
 
   Map<String, dynamic> toJson() => {
         'notifecm_id': notifEcmId,
         'nama': nama,
         'foto ': foto,
-        'status': status
+        'status': status,
+        'waktu': waktu
       };
 }
