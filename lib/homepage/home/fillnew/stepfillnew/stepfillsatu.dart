05d5f7f4d9c7e24bf864cadd62ee4330a86b25a9
Tezar <tezarm255@gmail.com>
2022-01-27 17:19:35 +0700

    commit stepfillsatu

--- lib/homepage/home/fillnew/stepfillnew/stepfillsatu.dart
+++ lib/homepage/home/fillnew/stepfillnew/stepfillsatu.dart
@@ -64,7 +64,7 @@ class StepFillSatuState extends State<StepFillSatu> {
   String next_two = '';
 
   String filterTeamMember = "";
-
+  
   void setBahasa() async {
     final prefs = await _prefs;
     String bahasaBool = prefs.getString("bahasa") ?? "";
