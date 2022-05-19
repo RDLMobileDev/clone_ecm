dc5fee09691794963ee5ee945c4dd92b99502b30
rizal ramdani <rizalarya1@gmail.com>
2022-05-19 17:38:46 +0700

    + update step 6 dan 8

--- lib/homepage/home/services/apifillnewdelapan.dart
+++ lib/homepage/home/services/apifillnewdelapan.dart
@@ -6,28 +6,33 @@ Future fillNewDelapan(String ecmId, String copyEngineer, String copyProduct,
     String copyOthers, String token) async {
   String myUrl = MyUrl().getUrlDevice();
   String url = "$myUrl/ecm_step8_insert";
-  http.Response response = await http.post(Uri.parse(url), headers: {
-    "Accept": "Application/json",
-    'Authorization': 'Bearer $token',
-  }, body: {
-    'ecm_id': ecmId,
-    'ecm_copyengineer': copyEngineer,
-    'ecm_copyproduct': copyProduct,
-    'ecm_copyothers': copyProduct
-  }).timeout(const Duration(seconds: 5));
 
-  // print(response.body);
-  // if (response.body != null) {
-  //   var convertData = jsonDecode(response.body);
-  //   return convertData;
-  // }
+  try {
+    http.Response response = await http.post(Uri.parse(url), headers: {
+      "Accept": "Application/json",
+      'Authorization': 'Bearer $token',
+    }, body: {
+      'ecm_id': ecmId,
+      'ecm_copyengineer': copyEngineer,
+      'ecm_copyproduct': copyProduct,
+      'ecm_copyothers': copyProduct
+    }).timeout(const Duration(seconds: 5));
 
-  if (response.statusCode == 200) {
-    var convertDatatoJson = jsonDecode(response.body);
-    json.decode(response.body);
-    return convertDatatoJson;
-  } else {
-    print("gagal total timeout");
+    // print(response.body);
+    // if (response.body != null) {
+    //   var convertData = jsonDecode(response.body);
+    //   return convertData;
+    // }
+
+    if (response.statusCode == 200) {
+      var convertDatatoJson = jsonDecode(response.body);
+      json.decode(response.body);
+      return convertDatatoJson;
+    } else {
+      print("gagal total timeout");
+    }
+  } catch (e) {
+    print(e);
   }
 }
 
